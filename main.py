e0cfd9437bb5420a23c7595c2304d00ff1adcd30
Carlo Duan <carloduan@Carlos-iMac.local>
2020-04-29 16:55:26 -0400

    changed write to a specific directory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
 
 def fetchHome(url):
@@ -7,14 +8,19 @@ def fetchHome(url):
     soup = BeautifulSoup(source.text, 'html.parser')
     book_title = soup.find_all(class_="title")
     book_list = soup.find_all(class_='book-list')
+    current_dir = os.getcwd()
+
     for index, book in enumerate(book_list):
         chapter_list = book.find_all('a')
-        # chapter_list = book.find_all('li').find_children()
         title = book_title[index].h3.a.string
-        f = open(title + '.txt', 'a')
+        os.mkdir(title)
+        dir = os.path.join(current_dir, title)
+        index = 0
         for chapter in chapter_list:
-            f.write(chapter.string + "\n")
-            # fetchChapter(f, chapter.get('href'))
+            f = open(os.path.join(dir, str(index) + '.txt'), 'a')
+            # f.write(chapter.string + "\n")
+            fetchChapter(f, chapter.get('href'))
+            index += 1
         print(f"Write {title} successfully!")
         f.close()
 
@@ -23,13 +29,13 @@ def fetchChapter(file, url):
     book_source = requests.get(url)
     soup = BeautifulSoup(book_source.text, 'html.parser')
     chapter_title = soup.find(id='nr_title').string
+    print(chapter_title + "\n")
+    # file.write(chapter_title + "\n")
+    p_list = soup.find(id='nr1').find_all('p')
+    writeToTxt(file, p_list)
 
-    file.write(chapter_title + "\n")
-    # p_list = soup.find(id='nr1').find_all('p')
-    # writeToTxt(file, chapter_title, p_list)
 
-
-def writeToTxt(file, title, paragraphs):
+def writeToTxt(file, paragraphs):
     for p in paragraphs:
         if p.string is not None:
             file.write(p.string + "\n")
